2bd77a4ff2d90ae39023698c240fe924fbddac18
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-09 08:59:18 +0000

    added some code, not working yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import AppHeader from "./components/AppHeader"
 
-
 function App() {
+  
   return (
     <>
       <AppHeader />
--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,24 @@
+import React from 'react';
+import { MouseEvent } from 'react';
+
 import "./AppHeader.css"
 
 function AppHeader(): JSX.Element {
+
+    const left: any = document.getElementById("left-side");
+
+    const handleMove = (e: MouseEvent) => {
+
+    const percentage = (e.clientX / window.innerWidth) * 100;
+
+    left.style.width = percentage.toFixed(2) + "%"
+    
+  }
+
+    document.onmousemove = e => handleMove(e);
     return (
         <div className="BothSides">
-            <div id="left-side" className="side">
+            <div id="left-side" className="side" onMouseMove={handleMove}>
                 <h2 className="title">
                     Today was <span className="fancy">good</span>
                 </h2>
