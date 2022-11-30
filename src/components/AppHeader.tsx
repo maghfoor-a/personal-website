68b54c40a5a1dafb4ec4df5530c0803ae738b1dc
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-30 11:49:13 +0000

    added website and linkedIn buttons

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,23 +7,30 @@ function AppHeader(): JSX.Element {
 
     const handleMove = (e: any) => {
 
-    const xPercentage = (e.clientX / window.innerWidth) * 100;
-    setWidthPct(xPercentage)
+        const xPercentage = (e.clientX / window.innerWidth) * 100;
+        setWidthPct(xPercentage)
     }
 
     return (
-        <div className="BothSides" onMouseMove={handleMove}>
-            <div id="left-side" className="side" style={{width: `${widthPct}%`}}>
-                <h2 className="title">
-                    Today was <span className="fancy">good</span>
-                </h2>
+        <>
+            <div className="BothSides" onMouseMove={handleMove}>
+                <div id="left-side" className="side" style={{ width: `${widthPct}%` }}>
+                    <h2 className="title">
+                        Today was <span className="fancy">GOOD</span>
+                    </h2>
+                    <a href="https://my-favourite-media.netlify.app/" target="_blank" rel="noopener noreferrer"><button className="button">My Favourite Media</button></a>
+                    <a href="https://github.com/maghfoor-a" target="_blank" rel="noopener noreferrer"><button className="button">GitHub</button></a>
+                </div>
+                <div id="right-side" className="side">
+                    <h2 className="title">
+                        Today was <span className="fancy">GREAT</span>
+                    </h2>
+                    <a href="https://my-favourite-media.netlify.app/" target="_blank" rel="noopener noreferrer"><button className="button">My Favourite Media</button></a>
+                    <a href="https://github.com/maghfoor-a" target="_blank" rel="noopener noreferrer"><button className="githubBtn">GitHub</button></a>
+                </div>
+                
             </div>
-            <div id="right-side" className="side">
-                <h2 className="title">
-                    Today was <span className="fancy">fantastic</span>
-                </h2>
-            </div>
-    </div>
+        </>
     )
 }
 export default AppHeader;
