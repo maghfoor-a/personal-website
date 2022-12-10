ee6f90e10ebc32bcbd9febaef05e534b9dac5d62
Maghfoor <maghfoor.ahmed05@gmail.com>
2022-12-10 13:53:55 +0000

    printed pictures in the creative section

--- src/components/CreativeCoding.tsx
+++ src/components/CreativeCoding.tsx
@@ -1,13 +1,28 @@
-import fish from "../utils/creativePictures/fish-gold.gif";
+import "./CreativeCoding.css";
+import { AllPictures, AllGifs } from "../utils/allPicturesArray";
 export default function CreativeCoding(): JSX.Element {
-  const media = [fish];
   return (
     <>
       <h1>My Creative Coding Journey!</h1>
       <h3>🚧🚧Work in progress! come back later🚧🚧</h3>
       <div className="allCreative">
-        {media.map((picture) => {
-          return <img src={picture} alt={picture} />;
+        {AllGifs.map((media) => {
+          return (
+            <div key={media.id} className="singlePicture">
+              <img src={media.gif} alt={media.gif} width="400" />
+              <h4>{media.name}</h4>
+            </div>
+          );
+        })}
+      </div>
+      <div className="allCreative">
+        {AllPictures.map((media) => {
+          return (
+            <div key={media.id} className="singlePicture">
+              <img src={media.picture} alt={media.picture} width="400" />
+              <h4>{media.name}</h4>
+            </div>
+          );
         })}
       </div>
     </>
---
+++ src/utils/allPicturesArray.ts
@@ -0,0 +1,90 @@
+//importing all the pictures that I have made
+import PicFirstProject from "../utils/creativePictures/FirstProject.png";
+import PicRandomWalk1 from "../utils/creativePictures/randomwalk (1).png";
+import PicRandomWalk2 from "../utils/creativePictures/randomwalk (3).png";
+import PicRandomWalk3 from "../utils/creativePictures/randomwalk (4).png";
+import PicRandomWalk4 from "../utils/creativePictures/randomwalk (7).png";
+import PicRandomWalk5 from "../utils/creativePictures/randomwalk (8).png";
+import PicRandomWalk6 from "../utils/creativePictures/randomwalk (10).png";
+import PicRandomWalk7 from "../utils/creativePictures/randomwalk (12).png";
+import PicRandomWalk8 from "../utils/creativePictures/randomwalk (13).png";
+import PicRandomWalk9 from "../utils/creativePictures/twoRandomWalkers.png";
+
+//importing gifs that I have made of my work
+import fishGold from "../utils/creativePictures/fish-gold.gif";
+import movingCircles from "../utils/creativePictures/moving-circles.gif";
+import starfield from "../utils/creativePictures/stars-colour.gif";
+
+//importing react 3D work gifs later on
+
+export const AllPictures = [
+  {
+    id: 1,
+    picture: PicFirstProject,
+    name: "My First Ever Project.",
+    link: "",
+  },
+  {
+    id: 2,
+    picture: PicRandomWalk1,
+    name: "Random Walker V1.",
+    link: "",
+  },
+  {
+    id: 3,
+    picture: PicRandomWalk3,
+    name: "Random Walker V2.",
+    link: "",
+  },
+  {
+    id: 4,
+    picture: PicRandomWalk9,
+    name: "Random Walker Adaptation: Two Random Walkers.",
+    link: "",
+  },
+  {
+    id: 5,
+    picture: PicRandomWalk2,
+    name: "Random Walker Adaptation C1.",
+    link: "",
+  },
+  {
+    id: 6,
+    picture: PicRandomWalk4,
+    name: "Random Walker Adaptation C2.",
+    link: "",
+  },
+  {
+    id: 7,
+    picture: PicRandomWalk5,
+    name: "Random Walker Adaptation C3.",
+    link: "",
+  },
+  {
+    id: 8,
+    picture: PicRandomWalk6,
+    name: "Random Walker Adaptation C4.",
+    link: "",
+  },
+];
+
+export const AllGifs = [
+  {
+    id: 1,
+    gif: fishGold,
+    name: "Testing with imported pictures, adapting from starfield.",
+    link: "",
+  },
+  {
+    id: 2,
+    gif: starfield,
+    name: "A 2D starfield built with speheres and strokes",
+    link: "",
+  },
+  {
+    id: 3,
+    gif: movingCircles,
+    name: "Plotting circles then added motion based on framecount.",
+    link: "",
+  },
+];
