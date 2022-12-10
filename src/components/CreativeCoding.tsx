0a14b5c2a15234872f2d0268f58f290ccb8d4c21
Maghfoor <maghfoor.ahmed05@gmail.com>
2022-12-10 18:14:05 +0000

    added title for skteches

--- src/components/CreativeCoding.tsx
+++ src/components/CreativeCoding.tsx
@@ -3,10 +3,13 @@ import { AllPictures, AllGifs } from "../utils/allPicturesArray";
 export default function CreativeCoding(): JSX.Element {
   return (
     <>
-      <div className="title">
+      <div className="Creativetitle">
         <h1>MY CREATIVE CODING JOURNEY</h1>
         <h3>🚧🚧Work in progress! come back later🚧🚧</h3>
         <p>please wait for the browser to fully load the media😊😊</p>
+      </div>
+      <div className="sketch">
+        <h1>2D ANIMATION and SKETCHES</h1>
       </div>
       <div className="allCreative">
         {AllGifs.map((media) => {
