3f7ff27204a14d87efbe25815d61be66d96f36e4
Maghfoor <maghfoor.ahmed05@gmail.com>
2022-12-10 15:02:50 +0000

    prettier

--- src/components/CreativeCoding.tsx
+++ src/components/CreativeCoding.tsx
@@ -3,16 +3,21 @@ import { AllPictures, AllGifs } from "../utils/allPicturesArray";
 export default function CreativeCoding(): JSX.Element {
   return (
     <>
-    <div className="title">
-      <h1>MY CREATIVE CODING JOURNEY</h1>
-      <h3>🚧🚧Work in progress! come back later🚧🚧</h3>
-      <p>please wait for the browser to full load the media😊😊</p>
-    </div>
+      <div className="title">
+        <h1>MY CREATIVE CODING JOURNEY</h1>
+        <h3>🚧🚧Work in progress! come back later🚧🚧</h3>
+        <p>please wait for the browser to full load the media😊😊</p>
+      </div>
       <div className="allCreative">
         {AllGifs.map((media) => {
           return (
             <div key={media.id} className="singlePicture">
-              <img className="image" src={media.gif} alt={media.gif} width="400" />
+              <img
+                className="image"
+                src={media.gif}
+                alt={media.gif}
+                width="400"
+              />
               <h4>{media.name}</h4>
             </div>
           );
@@ -22,7 +27,12 @@ export default function CreativeCoding(): JSX.Element {
         {AllPictures.map((media) => {
           return (
             <div key={media.id} className="singlePicture">
-              <img className="image" src={media.picture} alt={media.picture} width="400" />
+              <img
+                className="image"
+                src={media.picture}
+                alt={media.picture}
+                width="400"
+              />
               <h4>{media.name}</h4>
             </div>
           );
