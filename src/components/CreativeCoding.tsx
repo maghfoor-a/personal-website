d4aed2d4e394f5ffc2a647849c1060ce9285e934
Maghfoor <maghfoor.ahmed05@gmail.com>
2022-12-10 12:25:28 +0000

    added pictures to creative coding

--- src/components/CreativeCoding.tsx
+++ src/components/CreativeCoding.tsx
@@ -1,8 +1,15 @@
+import fish from "../utils/creativePictures/fish-gold.gif";
 export default function CreativeCoding(): JSX.Element {
+  const media = [fish];
   return (
     <>
       <h1>My Creative Coding Journey!</h1>
       <h3>🚧🚧Work in progress! come back later🚧🚧</h3>
+      <div className="allCreative">
+        {media.map((picture) => {
+          return <img src={picture} alt={picture} />;
+        })}
+      </div>
     </>
   );
 }
