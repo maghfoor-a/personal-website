61e26133159551ccc0acc5eaaddaddf93f719b3b
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-23 17:06:04 +0000

    made a seperate component for projects

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,10 +1,7 @@
 import "./AppHeader.css";
 import React from "react";
 import { Link } from "react-router-dom";
-import randomWalk from "../utils/creativePictures/randomwalk (10).webp";
-import MFM from "../utils/MFM.webp";
-import GetStuffDone from "../utils/GetStuffDone.webp";
-import ZMDB from "../utils/ZMDB.webp";
+import Projects from "./Projects";
 
 function AppHeader(): JSX.Element {
   const [widthPct, setWidthPct] = React.useState<number>(50);
@@ -45,51 +42,7 @@ function AppHeader(): JSX.Element {
             Today was <span className="fancy">GREAT</span>
           </h2>
         </div>
-        <div className="AllProjects">
-          <Link to="/creative">
-            <div className="button CC">
-              <img className="img" src={randomWalk} width={300} height={300} />
-              <p className="projectTitle">Creative Coding</p>
-            </div>
-          </Link>
-          <a
-            href="https://my-favourite-media.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="button MFM">
-              <img className="img" src={MFM} width={300} height={300} />
-              <p className="projectTitle">
-                My Favourite Media (work in progress!)
-              </p>
-            </div>
-          </a>
-          <a
-            href="https://getstuffdone.maghfoor.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="button GSD">
-              <img
-                className="img"
-                src={GetStuffDone}
-                width={300}
-                height={300}
-              />
-              <p className="projectTitle">Get Stuff Done!</p>
-            </div>
-          </a>
-          <a
-            href="https://zmdb-official.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="button ZMDB">
-              <img className="img" src={ZMDB} width={300} height={300} />
-              <p className="projectTitle">ZMDB</p>
-            </div>
-          </a>
-        </div>
+        <Projects />
       </div>
     </>
   );
