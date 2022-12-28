0688f0cd6b5e49efdbefeac3c5f5303d64890d13
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-28 21:57:28 +0000

    setup the index html header stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import AppHeader from "./components/AppHeader";
 import CreativeCoding from "./components/CreativeCoding";
 import { Route, Routes } from "react-router-dom";
+import MainBody from "./components/MainBody";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<AppHeader />}></Route>
+        <Route path="/" element={<MainBody />}></Route>
         <Route path="creative" element={<CreativeCoding />}></Route>
       </Routes>
     </>
---
+++ src/components/MainBody.tsx
@@ -0,0 +1,7 @@
+export default function MainBody(): JSX.Element {
+    return (
+        <>
+        <h1>Maghfoor Ahmed</h1>
+        </>
+    )
+}
