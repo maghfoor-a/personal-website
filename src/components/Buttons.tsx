7a0e7db4e2d02474e53dac54710cbadc0a1fa356
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-29 21:13:14 +0000

    ran yarn format

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,24 +1,24 @@
-export default function Buttons():JSX.Element {
-    return (
-        <>
-        <div className="extra">
-          <a
-            className="github"
-            href="https://github.com/maghfoor-a"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="button">GitHub</button>
-          </a>
-          <a
-            className="github"
-            href="https://www.linkedin.com/in/maghfoor"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="button">LinkedIn</button>
-          </a>
-        </div>
-        </>
-    )
+export default function Buttons(): JSX.Element {
+  return (
+    <>
+      <div className="extra">
+        <a
+          className="github"
+          href="https://github.com/maghfoor-a"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="button">GitHub</button>
+        </a>
+        <a
+          className="github"
+          href="https://www.linkedin.com/in/maghfoor"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="button">LinkedIn</button>
+        </a>
+      </div>
+    </>
+  );
 }
