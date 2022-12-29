ca5de92265646166a8788c63a50e552192e67ea1
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-29 20:51:05 +0000

    completed GetStuffDone page

--- src/components/ProjectPages/GetStuffDone.tsx
+++ src/components/ProjectPages/GetStuffDone.tsx
@@ -6,6 +6,9 @@ import { SiHtml5 } from 'react-icons/si'
 import { SiCss3 } from 'react-icons/si'
 import { SiNetlify } from 'react-icons/si'
 import { SiJest } from 'react-icons/si'
+import { SiPostgresql } from 'react-icons/si'
+import { SiFirebase } from 'react-icons/si'
+import { SiExpress } from 'react-icons/si'
 
 export default function GetStuffDone():JSX.Element {
     return (
@@ -16,7 +19,7 @@ export default function GetStuffDone():JSX.Element {
             <button className='project_button-live' onClick={() => window.open("https://getstuffdone.maghfoor.com/login", "_self")}>Live Site</button>
             <button className='project_button-code' onClick={() => window.open("https://github.com/maghfoor-a/fullstack-todo-frontend", "_self")}>See The Code</button>
         </div>
-        <p className="project_description">I developed a React web application that utilizes the <b>Spotify API</b> to allow users to search for artists and their albums, and to save their favorite albums. The goal of the app was to provide a simple and intuitive solution for storing and organizing music, as existing options did not meet my needs. The app leverages the Spotify API to offer a comprehensive and user-friendly experience for music enthusiasts.</p>
+        <p className="project_description">Get Stuff Done is a web application that helps users stay organized and on top of their tasks. The front end is built using <b>React</b> and <b>Typescript</b>, and the backend is built using <b>Express</b> and Typescript. The site is deployed on <b>Netlify</b> and uses <b>Firebase</b> for authentication and a <b>postgreSQL</b> database. The project was built using <b>Git</b> for version control and <b>Jest</b> for testing. This project has been a valuable learning experience for me, allowing me to gain experience in building a full-stack web application with authentication and a database.</p>
         </section>
         <section>
             <p className="project_subheading">TECH STACK</p>
@@ -52,6 +55,18 @@ export default function GetStuffDone():JSX.Element {
                 <div className='single-icon'>
                 <SiJest size={'3rem'} color={"#C53D14"}/>
                 <p className='project_icon-name'>Jest</p>
+                </div>
+                <div className='single-icon'>
+                <SiPostgresql size={'3rem'} color={"#0064a5"}/>
+                <p className='project_icon-name'>PostgreSQL</p>
+                </div>
+                <div className='single-icon'>
+                <SiFirebase size={'3rem'} color={'#FFA611'}/>
+                <p className='project_icon-name'>FireBase</p>
+                </div>
+                <div className='single-icon'>
+                <SiExpress size={'3rem'} color={'black'}/>
+                <p className='project_icon-name'>Express</p>
                 </div>
             </div>
         </section>
